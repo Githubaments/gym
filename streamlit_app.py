8484866b87db18d3bbf0e167d0465212c48d7bbe
Githubaments <flynnshane@gmail.com>
2023-04-10 15:28:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,19 @@ sheet = gc.open('Gym Log').sheet1
 # Read the data from the sheet
 data = sheet.get_all_records()
 
+# Extract the workout names
+workouts = list(set([d['Workout'] for d in data]))
+
+# Allow the user to choose a workout to filter by
+selected_workout = st.sidebar.selectbox('Select a workout', workouts)
+
+# Filter the data by the selected workout
+workout_data = [d for d in data if d['Workout'] == selected_workout]
+
+# Extract the exercises, weights, and sets data
+exercises = [d['Exercise'] for d in workout_data]
+weights = [[d['Set 1'], d['Set 2'], d['Set 3']] for d in workout_data]
+
 # Display the data in a Streamlit app
-st.write(data)
+st.write('Exercises:', exercises)
+st.write('Weights:', weights)
