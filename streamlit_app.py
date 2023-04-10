2d1b7240ff89905e837a06b748a3c76de2b4b4fe
Githubaments <flynnshane@gmail.com>
2023-04-10 13:02:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,19 @@
-import gspread
-from google.oauth2.service_account import Credentials
 import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
 
-# Authenticate and authorize the API client with your service account key
-creds = Credentials.from_service_account_file('path/to/your/service_account_key.json')
-client = gspread.authorize(creds)
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
 
 # Open the Google Sheet by name
-sheet = client.open('MySheetName').sheet1
+sheet = conn.open('MySheetName').sheet1
 
 # Read the data from the sheet
 data = sheet.get_all_records()
