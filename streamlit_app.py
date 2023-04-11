4504cd91e7c12595cf3dce0bb67769e1e5f77498
Githubaments <flynnshane@gmail.com>
2023-04-12 00:08:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,8 +87,11 @@ for exercise in df['Exercise'].unique():
             'set2': int(set2) if isinstance(set2, (int, float)) else 0,
             'set3': int(set3) if isinstance(set3, (int, float)) else 0,
         }
+        try:
+            weight = st.number_input('Weight', value=previous_values[exercise]['weight'],step=1.0, key=f'{exercise}-weight')
 
-        weight = st.number_input('Weight', value=previous_values[exercise]['weight'], key=f'{exercise}-weight')
+        except:
+            weight = st.number_input('Weight', value=previous_values[exercise]['weight'], key=f'{exercise}-weight')
         set1 = st.number_input('Set 1', value=previous_values[exercise]['set1'], key=f'{exercise}-set1')
         set2 = st.number_input('Set 2', value=previous_values[exercise]['set2'], key=f'{exercise}-set2')
         set3 = st.number_input('Set 3', value=previous_values[exercise]['set3'], key=f'{exercise}-set3')
